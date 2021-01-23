ab93b7e983ef808da4b6e4309a4e5810ca33f744
Jakub Piszczan <jpiszczan@gmail.com>
2021-01-24 00:32:52 +0100

    accounts list

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,4 +2,5 @@ import { StickyProfileGuideAnimations } from "./animations";
 import { Users } from "./users";
 
 window.addEventListener("scroll", () => { StickyProfileGuideAnimations(); });
-
+const xyz = Users.initUsers();
+console.log(xyz[0]);
